68d7cf76e5f9011212d723be616399e5e4dccb3c
Damir2302 <damir23.02@mail.ru>
2022-09-26 19:37:47 +0300

    fixed

--- src/assets/js/sliders.js
+++ src/assets/js/sliders.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
     let heroSlider = new Swiper('.hero-slider', {
         slidesPerView: 1,
         spaceBetween: 50,
+        effect: 'fade',
 
         pagination: {
             el: '.swiper-pagination',
+            clickable: true
           },
 
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.hero-slider-buttons .button-next',
+            prevEl: '.hero-slider-buttons .button-prev',
         }
     })
 
