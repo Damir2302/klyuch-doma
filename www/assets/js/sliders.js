9c88e2ccc1874e357119695f83b2bc2bd0b606fc
Damir2302 <damir23.02@mail.ru>
2022-09-27 18:14:38 +0300

    main page

---
+++ www/assets/js/sliders.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    let heroSlider = new Swiper('.hero-slider', {
+        slidesPerView: 1,
+        spaceBetween: 50,
+        effect: 'fade',
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+
+        navigation: {
+            nextEl: '.hero-slider-buttons .button-next',
+            prevEl: '.hero-slider-buttons .button-prev',
+        }
+    })
+
+});
