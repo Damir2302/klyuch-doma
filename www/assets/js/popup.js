eed4da7c976a9ffbd35e4bf8de7bc054d80baf48
Damir2302 <damir23.02@mail.ru>
2022-10-28 18:39:03 +0300

    fixed

--- www/assets/js/popup.js
+++ www/assets/js/popup.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
 
-    // ДЛЯ ВСПЛЫВАЮЩИХ ОКОН
+    // ДЛЯ ВСЕХ(!!!!!!) ВСПЛЫВАЮЩИХ ОКОН [Если кликаем вне всплывающего поля, представленных ниже]
     $('html').on('click', function(e) {
         if (!$(e.target).closest('#menu').length &&
             !$(e.target).closest('.popup__inner').length &&
-            !$(e.target).closest('#project-dwld').length) {
+            !$(e.target).closest('#project-dwld').length &&
+            !$(e.target).closest('.c-price__item-spec').length &&
+            !$(e.target).closest('.c-price-s-inner').length) {
             $('body').removeClass('overflow-hidden')
             $('#page').removeClass('bg-overlay')
             $('#menu').removeClass('active')
             $('.popup').removeClass('show')
             $('.popup__inner').removeClass('active')
+            $('.c-price-specification').removeClass('show')
         }
     })
 
+    // ДЛЯ POPUP
     $('#popup-close').on('click', function() {
         $('body').removeClass('overflow-hidden')
         $('#page').removeClass('bg-overlay')
@@ -26,6 +30,19 @@ $(document).ready(function() {
         $('#page').addClass('bg-overlay')
         $('.popup').addClass('show')
         $('.popup__inner').addClass('active')
+    })
+
+    // СТРАНИЦА CATEGORY ЦЕНЫ ПРОЕКТА
+    $('.c-price__item-spec').on('click', function() {
+        $('body').addClass('overflow-hidden')
+        $('#page').addClass('bg-overlay')
+        $(`.c-price-specification[data-spec-active="${$(this).attr('data-spec')}"]`).addClass('show')
+    })
+
+    $('.c-price-specification-close').on('click', function() {
+        $('body').removeClass('overflow-hidden')
+        $('#page').removeClass('bg-overlay')
+        $('.c-price-specification').removeClass('show')
     })
 
 });
